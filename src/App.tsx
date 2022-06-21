c0ee19d9b5ce166a94195fc29db2a937c1459aeb
devwedney1 <devwedney@outlook.com>
2022-06-21 09:33:15 +0100

    init 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
+import Landing from './pages/landing';
+
 import './assets/styles/global.css'
 
 function App() {
   return (
-    <div className="App">
-    <h1>Heloo World </h1>
-    </div>
+   <Landing />
   );
 }
 
