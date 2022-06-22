a7f1087f9fd7b53d304074ea4b2fe5fcd779b08e
devwedney1 <devwedney@outlook.com>
2022-06-22 21:22:31 +0100

    init 5

--- src/pages/landing/TeacherList/index.tsx
+++ src/pages/landing/TeacherList/index.tsx
@@ -7,7 +7,24 @@ function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponiveis.">
-        teste
+        <form id="search-teachers">
+          <div className="input-block">
+            <div>
+              <label htmlFor="subject">Materia</label>
+              <input type="text" id="subject" />
+            </div>
+
+            <div id="search-teachers">
+              <label htmlFor="week_day">Dia da Semana</label>
+              <input type="text" id="week_day" />
+            </div>
+
+            <div id="search-teachers">
+              <label htmlFor="time">Hora</label>
+              <input type="text" id="time" />
+            </div>
+          </div>
+        </form>
       </PageHeader>
     </div>
   );
